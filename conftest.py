aa02dd9d2ead410b4d8fe77fb7b24a5a09342b55
Mauko Quiroga <hello@mauko.me>
2021-09-03 15:52:00 +0200

    Fixed core/fixtures

--- conftest.py
+++ conftest.py
@@ -1,6 +1,5 @@
 pytest_plugins = [
-    "tests.fixtures.appclient",
-    "tests.fixtures.entities",
-    "tests.fixtures.simulations",
-    "tests.fixtures.taxbenefitsystems",
+    "openfisca_core.tests.fixtures.entities",
+    "openfisca_core.tests.fixtures.simulations",
+    "openfisca_core.tests.fixtures.taxbenefitsystems",
     ]
