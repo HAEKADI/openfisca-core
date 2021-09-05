36b6d6124dd461f758c27455ba9523f605c12d5c
Mauko Quiroga <hello@mauko.me>
2021-09-05 20:03:57 +0200

    Fix failing tests

--- openfisca_core/types/data_types/__init__.py
+++ openfisca_core/types/data_types/__init__.py
@@ -1,2 +1,2 @@
 from .arrays import ArrayLike, ArrayType  # noqa: F401
-from .roles import Subroles, RoleLike, Roles  # noqa: F401
+from .roles import RoleLike  # noqa: F401
--- openfisca_core/types/data_types/roles.py
+++ openfisca_core/types/data_types/roles.py
@@ -1,8 +1,21 @@
-from typing import Dict, List, Union
+from typing import Iterable, Optional
+from typing_extensions import TypedDict
 
 
-Subroles = List[str]
+class RoleLike(TypedDict, total = False):
+    """Base type for any data castable to a role-like model.
 
-RoleLike = Dict[str, Union[str, Subroles]]
+    Type-checking against abstractions rather than implementations helps in
+    (a) decoupling the codebse, thanks to structural subtyping, and
+    (b) documenting/enforcing the blueprints of the different OpenFisca models.
 
-Roles = List[RoleLike]
+    .. versionadded:: 35.5.0
+
+    """
+
+    key: str
+    plural: Optional[str]
+    label: Optional[str]
+    doc: Optional[str]
+    max: Optional[int]
+    subroles: Optional[Iterable[str]]
