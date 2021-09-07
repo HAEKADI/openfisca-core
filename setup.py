70390760e06bc946e5769972a36adf468f6fa3b4
Mauko Quiroga <hello@mauko.me>
2021-09-08 00:08:39 +0200

    Announce support to what's tested (3.7)

--- setup.py
+++ setup.py
@@ -33,8 +33,6 @@ setup(
         'Operating System :: POSIX',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
         'Topic :: Scientific/Engineering :: Information Analysis',
         ],
     description = 'A versatile microsimulation free software',
