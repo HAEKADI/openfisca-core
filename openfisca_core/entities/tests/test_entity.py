b3f067e31aae427458b0e2a1bdfd58fef5767621
Mauko Quiroga <hello@mauko.me>
2021-09-11 21:26:34 +0200

    Add missing doc to entities

--- openfisca_core/entities/tests/test_entity.py
+++ openfisca_core/entities/tests/test_entity.py
@@ -1,19 +1,54 @@
 import pytest
 
 from openfisca_core.entities import Entity, Role
+from openfisca_core.errors import VariableNotFoundError
 from openfisca_core.taxbenefitsystems import TaxBenefitSystem
+from openfisca_core.variables import Variable
 
 
 @pytest.fixture
 def entity():
     """An entity."""
+
     return Entity("key", "label", "plural", "doc")
 
 
 @pytest.fixture
 def role(entity):
     """A role."""
+
     return Role({"key": "key"}, entity)
+
+
+@pytest.fixture
+def MyVariable(entity):
+    """A variable."""
+
+    return type(
+        "MyVariable",
+        (Variable,),
+        {
+            "definition_period": "month",
+            "value_type": float, "entity":
+            entity,
+            },
+        )
+
+
+@pytest.fixture
+def tax_benefit_system(entity, MyVariable):
+    """A tax-benefit system."""
+
+    tbs = TaxBenefitSystem([entity])
+    tbs.load_variable(MyVariable)
+    return tbs
+
+
+@pytest.fixture
+def method(tax_benefit_system):
+    """A descriptable method."""
+
+    return tax_benefit_system.get_variable
 
 
 def test_variable_when_not_set(entity):
@@ -29,11 +64,11 @@ def test_variable_query_when_not_set(entity):
         entity.variable("variable")
 
 
-def test_set_tax_benefit_system_deprecation(entity):
+def test_set_tax_benefit_system_deprecation(entity, tax_benefit_system):
     """:meth:`.set_tax_benefit_system` throws a deprecation warning."""
 
     with pytest.warns(DeprecationWarning):
-        entity.set_tax_benefit_system(TaxBenefitSystem([entity]))
+        entity.set_tax_benefit_system(tax_benefit_system)
 
 
 def test_check_role_validity_deprecation(entity, role):
@@ -48,3 +83,28 @@ def test_get_variable_deprecation(entity):
 
     with pytest.warns(DeprecationWarning):
         entity.get_variable("variable")
+
+
+def test_check_variable_defined_for_entity_when_no_descriptor(entity):
+    """Returns None when there is no descriptor set."""
+
+    assert not entity.check_variable_defined_for_entity("asdf")
+
+
+def test_check_variable_defined_for_entity_when_no_variable(entity, method):
+    """Raises VariableNotFoundError when the variable is not found."""
+
+    entity.variable = method
+
+    with pytest.raises(VariableNotFoundError):
+        entity.check_variable_defined_for_entity("asdf")
+
+
+def test_check_variable_defined_for_entity_when_not_hers(method):
+    """Raises ValueError when variable found is not hers."""
+
+    entity = Entity("extra-key", "label", "plural", "doc")
+    entity.variable = method
+
+    with pytest.raises(ValueError):
+        entity.check_variable_defined_for_entity("MyVariable")
